6288648dfc0b01bc7dd30c54091e8bab2447663d
jkim1373 <jkim1373@ucr.edu>
2025-11-08 22:32:55 +0000

    Inclass5

--- loops.sh
+++ loops.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 fruit=("apples" "orange" "strawberry" "pineaple" "guava")
+
+
+for ((i=10; i>-o; i--)); do
+    echo $i
+done
+
+n=0
+while [ $n -lt 10 ]; do
+    n=$((n+1));
+done
+echo "$n"
+
+
+for item in ${fruitx[@]}; do
+    echo $item
+done
---
+++ variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+first_name="Justin"
+last_name="Kim"
+echo $first_name $last_name
+current_date=$(date)
+echo "Hello ${first_name} ${last_name} today is ${current_date}"
+
